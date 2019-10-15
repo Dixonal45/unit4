7c8979e17a98f99b7e8949aeba0c846c9d0034eb
allison <allison.dixon@ssfs.org>
2019-10-15 10:15:16 -0400

    working on project euler #3

--- testing.py
+++ testing.py
@@ -1,24 +1,29 @@
-# project euler problems
+# # project euler problems
+#
+# # project euler problem 1 (multiples of 3 and 5)
+#
+# multiples = 0
+# for x in range(3, 1000):
+#     if (x % 3) == 0 or (x % 5) == 0:
+#         multiples = multiples + x
+#
+# print(multiples)
+#
+# # project euler problem 2 (even fibonacci numbers)
+#
+# sum = 0
+# a = 1
+# b = 1
+# for x in range(1, 500):
+#     c = a + b
+#     if (c % 2) == 0 and c <= 4000000:
+#         sum = sum + c
+#     a = b
+#     b = c
+#
+# print(sum)
 
-# project euler problem 1 (multiples of 3 and 5)
+# project euler problem 3
 
-multiples = 0
-for x in range(3, 1000):
-    if (x % 3) == 0 or (x % 5) == 0:
-        multiples = multiples + x
-
-print(multiples)
-
-# project euler problem 2 (even fibonacci numbers)
-
-sum = 0
-a = 1
-b = 1
-for x in range(1, 500):
-    c = a + b
-    if (c % 2) == 0 and c <= 4000000:
-        sum = sum + c
-    a = b
-    b = c
-
-print(sum)
+root = (float(600851475143 ** (1/2)))
+for x in range(1, root):
